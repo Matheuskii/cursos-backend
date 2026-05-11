7f3f422fa1e1b272e32a6c6d78585e11e27a3b98
Matheus <alvesdemelomatheus4@gmail.com>
2026-05-11 09:34:22 -0300

    feat: implementa o get por ID e padroniza o retorno com ResponseEntity

--- src/main/java/br/com/senai/cursosBackend/controllers/CursoController.java
+++ src/main/java/br/com/senai/cursosBackend/controllers/CursoController.java
@@ -1,16 +1,21 @@
 package br.com.senai.cursosBackend.controllers;
 
 
-import br.com.senai.cursosBackend.curso.Curso;
-import br.com.senai.cursosBackend.curso.CursoRepository;
-import br.com.senai.cursosBackend.curso.DadosListagemCurso;
+import br.com.senai.cursosBackend.curso.*;
+import jakarta.validation.Valid;
+import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("cursos")
@@ -20,9 +25,22 @@ public class CursoController {
     CursoRepository repository;
 
     @GetMapping
-    private List<DadosListagemCurso> listarCursos(){
+    private ResponseEntity<List<DadosListagemCurso>> listarCursos(){
         List<Curso> cursos = repository.findAllByAtivoTrue();
-        return cursos.stream().map(DadosListagemCurso::new).toList();
+        List<DadosListagemCurso> cursosDTO =  cursos.stream().map(DadosListagemCurso::new).toList();
 
+        return ResponseEntity.ok().body(cursosDTO);
     }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<DadosDetalhesCurso> listarCursosPorId(@PathVariable Long id){
+        Optional<Curso> curso = Optional.of(repository.findByIdAndAtivoTrue(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado esse curso")));
+        DadosDetalhesCurso cursoDTO = new DadosDetalhesCurso(curso);
+
+
+
+        return ResponseEntity.ok().body(cursoDTO);
+    }
+
+
 }
--- src/main/java/br/com/senai/cursosBackend/curso/CursoRepository.java
+++ src/main/java/br/com/senai/cursosBackend/curso/CursoRepository.java
@@ -3,10 +3,12 @@ package br.com.senai.cursosBackend.curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
     List<Curso> findAllByAtivoTrue();
 
+    Optional<Curso> findByIdAndAtivoTrue(Long id);
 }
